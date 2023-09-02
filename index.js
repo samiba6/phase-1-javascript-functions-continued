93e3fc91c4f8e7ed12440dc80582326e5f00e018
Sami <s.babouche@esi-sba.dz>
2023-09-02 14:29:03 +0100

    Completed Assignment

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 // code your solution here
+// code your solution here
+function saturdayFun(act="roller-skate"){
+
+    return `This Saturday, I want to ${act}!`;
+}
+const mondayWork= function (act="go to the office"){ return `This Monday, I will ${act}.`}
+function wrapAdjective(flair="*"){
+  return  function inner(par="special"){
+        return  `You are ${flair}${par}${flair}!`;
+    }
+}
+console.log(wrapAdjective()())
